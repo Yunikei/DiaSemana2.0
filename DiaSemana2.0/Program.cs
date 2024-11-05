cffd7f2f24ec60f426586451611c7ec00cd0b483
Yunkei <alva_alcantar24@unikino.edu.mx>
2024-11-05 07:24:54 -0600

    Agregar archivos de proyecto.

---
+++ DiaSemana2.0/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace DiaSemana2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dame un número");
+            string dato = Console.ReadLine();
+
+            int dia = 0;
+            bool sePudoConvertir = false;
+            sePudoConvertir = int.TryParse(dato, out dia);
+            if (sePudoConvertir)
+            {
+                switch (dia)
+                {
+                    case 1:
+                        Console.WriteLine("Lunes");
+                        break;
+                    case 2:
+                        Console.WriteLine("Martes");
+                        break;
+                    case 3:
+                        Console.WriteLine("Miercoles");
+                        break;
+                    case 4:
+                        Console.WriteLine("Jueves");
+                        break;
+                    case 5:
+                        Console.WriteLine("Viernes");
+                        break;
+                    case 6:
+                        Console.WriteLine("Sabado");
+                        break;
+                    case 7:
+                        Console.WriteLine("Domingo");
+                        break;
+                    default:
+                        Console.WriteLine("$Dato incorrecto (switch): {dato}");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"El dato introducido es incorrecto: {dato}");
+            }
+        }
+    }
+}
